4e477941ef1cb3a6863caa0c8e22a065f62230ce
CandiceJoy <candice@candicejoy.com>
2022-02-17 19:27:49 -0600

    Updated with the live folder structure from server.

--- app.js
+++ app.js
@@ -3,13 +3,10 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-
 const phasmoRouter = require('./routes/phasmo.js');
-const usersRouter = require('./routes/users');
-
+const logger = require('morgan');
 const app = express();
-
+console.log("Node WWW Server Start");
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -18,10 +15,26 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/phasmo', phasmoRouter);
-app.use('/users', usersRouter);
+/*
+To add sites
+------------
+Put site statics in public/sitename
+Put views in views/sitename
+Create a router in routes (based off phasmo.js)
+Add const sitenameRouter = require("./routes/sitename"); at the top of this file
+Add an app.use statement below, using app.use(/^\/sitename\/?$/,sitenameRouter);
+Lastly, add a location to /etc/nginx/sites-available/default in the following format (yes, node.conf works for all apps served by this app)
+  location ^~ /sitename {
+    include node.conf;
+  }
+*/
+
+const dir = path.join(__dirname, "public");
+console.log(dir);
+
+app.use(express.static(dir));
+app.use(/^\/phasmo\/?$/, phasmoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/phasmo.js
+++ routes/phasmo.js
@@ -2,8 +2,8 @@
 const express = require('express');
 const router = express.Router();
 const fs = require('fs');
-
-const raw = fs.readFileSync('public/ghosts.json');
+const path = require('path');
+const raw = fs.readFileSync('public/phasmo/ghosts.json');
 const json= JSON.parse(raw.toString());
 const evidence = json["evidence"];
 const evidenceOut = {};
@@ -20,7 +20,7 @@ const ghosts = json["ghosts"];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: "Candice's Phasmophobia Evidence Tracker", ghosts:ghosts, evidence:evidenceOut });
+  res.render('phasmo/index', { title: "Candice's Phasmophobia Evidence Tracker", ghosts:ghosts, evidence:evidenceOut });
 });
 
 module.exports = router;
